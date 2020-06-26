3a1d45f90007bdade796139932196629f0f7bbe6
namsonx <namsonx@gmail.com>
2020-06-26 07:48:59 +0700

    added simulate data

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,23 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,7 +33,9 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    bar_labels=labels
+    bar_values=values
+    return render_template('index.html', title='Home', user=user, posts=posts, max=17000, labels=bar_labels, values=bar_values)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
---
+++ data_simulator.py
@@ -0,0 +1,66 @@
+import mysql.connector as sql
+import random
+
+
+def simulator(data, header_id):
+    try:
+        conn = sql.connect(user='root', password='Son@123456',
+                                host='localhost', database='miniproject')
+        cursor = conn.cursor()
+    except sql.Error as err:
+        if err.errno == sql.errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == sql.errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    #print("testing: ", dir(cursor))
+    query = """
+        INSERT INTO sensor_data (data_value, sensor_header_id) VALUES (%s, %s)
+    """
+    val = (data, header_id)
+    try:
+        cursor.execute(query, val)
+        conn.commit()
+    except sql.Error as err:
+        print(err)
+
+def main():
+    humidity_range = []
+    for i in range(15,27):
+        humidity_range.append(i)
+    print(humidity_range)
+    tmp_range = []
+    for i in range(20,45):
+        tmp_range.append(i)
+    print(tmp_range)
+    light_range = []
+    for i in range(0,100):
+        light_range.append(i)
+    print(light_range)
+    i=1
+    while i<31:
+        humidity = random.choice(humidity_range)
+        simulator(humidity, 1)
+        temp = random.choice(tmp_range)
+        simulator(temp, 2)
+        light = random.choice(light_range)
+        simulator(light, 3)
+        humidity = random.choice(humidity_range)
+        simulator(humidity, 4)
+        temp = random.choice(tmp_range)
+        simulator(temp, 5)
+        light = random.choice(light_range)
+        simulator(light, 6)
+        humidity = random.choice(humidity_range)
+        simulator(humidity, 7)
+        temp = random.choice(tmp_range)
+        simulator(temp, 8)
+        light = random.choice(light_range)
+        simulator(light, 9)
+
+        i=i+1    
+
+if __name__=="__main__":
+    main()
+    pass
